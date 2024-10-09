384b30031d02e1753c1e654043086da352ff792f
Raffaela <raffa.fdo@gmail.com>
2024-10-08 21:43:28 -0300

    atualizado

--- EXERCICIOS/ex003.js
+++ EXERCICIOS/ex003.js
@@ -1,13 +1,8 @@
 //A)
-function x() {
-    let nome = 'raffaela'
-    let idade = 18
-    let cidade = 'são leopoldo'
-    let estudante = 'sou estudante'
-    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e ${estudante}.`)
+
+//B) 
+function infoPessoa(nome, idade, endereco, profissao){
+
+ console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${endereco} e sou ${profissao}.`)
 }
-let res =  x
-console.log(res)
-
-
-//B)
+infoPessoa('raffaela', 18, 'sao leopoldo', 'estudante')
