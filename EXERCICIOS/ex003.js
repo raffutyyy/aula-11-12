254f120fbceeeb543f52fe7c4bc60b62e9329f99
Raffaela Fernandes <161386357+raffutyyy@users.noreply.github.com>
2024-10-12 12:24:01 -0300

    atualizado

--- EXERCICIOS/ex003.js
+++ EXERCICIOS/ex003.js
@@ -1,4 +1,13 @@
 //A)
+function info(){
+    let nome = 'raffaela'
+    let idade = 18
+    let endereco = 'sao leopoldo'
+    let profissao = 'estudante'
+    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${endereco} e sou ${profissao}`)
+}
+info()
+
 
 //B) 
 function infoPessoa(nome, idade, endereco, profissao){
